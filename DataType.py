0dda74793e583cef86003c2e86d6f7b7325c438d
AzlanHassan24 <s.masoodukpk@gmail.com>
2025-02-21 17:43:55 +0000

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,11 @@
+print("Enter Your Name: ")
+name = input("enter your name: ")
+
+print("\nHello",name,"\nwelcome to codingal")
+
+print("Enter Your Age: ")
+age = input("enter your age: ")
+
+print("\nHi",age,"\nWelcome to UK")
+
+
